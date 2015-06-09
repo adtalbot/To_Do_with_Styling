f904ca48ce44e725633af1b72c26f98a20d98428
audrie <audrie.talbot@gmail.com>
2015-06-09 11:27:34 -0700

    Passes spec for save and eql methods for task class.

--- lib/task.rb
+++ lib/task.rb
@@ -5,6 +5,7 @@ class Task
     @description = attributes.fetch(:description)
     @list_id = attributes.fetch(:list_id)
   end
+
   define_singleton_method(:all) do
     returned_tasks = DB.exec('SELECT * FROM tasks;')
     tasks = []
@@ -14,5 +15,13 @@ class Task
       tasks.push(Task.new({:description => description, :list_id => list_id}))
     end
     tasks
+  end
+
+  define_method(:save) do
+    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
+  end
+
+  define_method(:==) do |another_task|
+    self.description().==(another_task.description()).&(self.list_id.==(another_task.list_id()))
   end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -8,9 +8,25 @@ describe(Task) do
       expect(test_task.description()).to(eq("Wash the car."))
     end
   end
+
   describe('.all') do
     it('is empty at first') do
       expect(Task.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves an instance of task to an array') do
+      test_task = Task.new(:description => "Fun", :list_id => 1)
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
+    end
+  end
+  describe('#==') do
+    it('is the same task if it has the same description and list_id') do
+      test_task = Task.new({:description => 'Wash the dog', :list_id => 1})
+      test_task2 = Task.new({:description => 'Wash the dog', :list_id => 1})
+      expect(test_task).to(eq(test_task2))
     end
   end
 end
