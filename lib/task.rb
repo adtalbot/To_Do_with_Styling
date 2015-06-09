1a3a92905590b87d930edf617059b37cfeb9480a
audrie <audrie.talbot@gmail.com>
2015-06-09 11:08:09 -0700

    Passes spec for all method for task class.

---
+++ lib/task.rb
@@ -0,0 +1,18 @@
+class Task
+  attr_reader(:description, :list_id)
+
+  define_method(:initialize) do |attributes|
+    @description = attributes.fetch(:description)
+    @list_id = attributes.fetch(:list_id)
+  end
+  define_singleton_method(:all) do
+    returned_tasks = DB.exec('SELECT * FROM tasks;')
+    tasks = []
+    returned_tasks.each() do |task|
+      description = task.fetch('description')
+      list_id = task.fetch('list_id').to_i()
+      tasks.push(Task.new({:description => description, :list_id => list_id}))
+    end
+    tasks
+  end
+end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,1 +1,16 @@
 require('spec_helper')
+
+
+describe(Task) do
+  describe('#description') do
+    it('returns the description of a instance of task') do
+      test_task = Task.new(:description => "Wash the car.", :list_id => 1)
+      expect(test_task.description()).to(eq("Wash the car."))
+    end
+  end
+  describe('.all') do
+    it('is empty at first') do
+      expect(Task.all()).to(eq([]))
+    end
+  end
+end
